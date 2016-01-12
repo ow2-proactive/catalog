85ee46aad9b22aab03c9941e78831e6daf7f5b76
Laurent Pellegrino <laurent.pellegrino@activeeon.com>
2016-01-12 16:31:04 +0100

    Add integration tests for get workflow resource

--- src/integration-test/java/org/ow2/proactive/workflow_catalog/rest/controller/WorkflowControllerIntegrationTest.java
+++ src/integration-test/java/org/ow2/proactive/workflow_catalog/rest/controller/WorkflowControllerIntegrationTest.java
@@ -30,13 +30,17 @@
  */
 package org.ow2.proactive.workflow_catalog.rest.controller;
 
+import com.google.common.io.ByteStreams;
+import com.jayway.restassured.response.Response;
 import org.apache.http.HttpStatus;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ow2.proactive.workflow_catalog.rest.Application;
 import org.ow2.proactive.workflow_catalog.rest.InMemoryConfiguration;
+import org.ow2.proactive.workflow_catalog.rest.dto.WorkflowMetadata;
 import org.ow2.proactive.workflow_catalog.rest.entity.Bucket;
+import org.ow2.proactive.workflow_catalog.rest.service.WorkflowService;
 import org.ow2.proactive.workflow_catalog.rest.service.repository.BucketRepository;
 import org.ow2.proactive.workflow_catalog.rest.service.repository.WorkflowRepository;
 import org.ow2.proactive.workflow_catalog.rest.util.IntegrationTestUtil;
@@ -45,6 +49,9 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.IOException;
+import java.util.Arrays;
 
 import static com.jayway.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.hasSize;
@@ -69,11 +76,18 @@ public class WorkflowControllerIntegrationTest {
     @Autowired
     private WorkflowRepository workflowRepository;
 
+    @Autowired
+    private WorkflowService workflowService;
+
     private Bucket bucket;
 
+    private WorkflowMetadata workflow;
+
     @Before
-    public void setup() {
+    public void setup() throws IOException {
         bucket = bucketRepository.save(new Bucket("myBucket"));
+        workflow = workflowService.createWorkflow(
+                bucket.getId(), IntegrationTestUtil.getWorkflowAsByteArray("workflow.xml"));
     }
 
     @Test
@@ -83,7 +97,7 @@ public class WorkflowControllerIntegrationTest {
                 .when().post(WORKFLOWS_RESOURCE).then()
                 .assertThat().statusCode(HttpStatus.SC_CREATED)
                 .body("bucket_id", is(bucket.getId().intValue()))
-                .body("id", is(1))
+                .body("id", is(2))
                 .body("name", is("Valid Workflow"))
                 .body("project_name", is("Project Name"))
                 .body("revision", is(1))
@@ -111,6 +125,71 @@ public class WorkflowControllerIntegrationTest {
         given().pathParam("bucketId", 42)
                 .multiPart(IntegrationTestUtil.getWorkflowFile("workflow.xml"))
                 .when().post(WORKFLOWS_RESOURCE).then()
+                .assertThat().statusCode(HttpStatus.SC_NOT_FOUND);
+    }
+
+    @Test
+    public void testGetWorkflowMetadataShouldReturnSavedWorkflowMetadata() {
+        given().pathParam("bucketId", 1).pathParam("workflowId", 1)
+                .when().get(WORKFLOW_RESOURCE).then()
+                .assertThat().statusCode(HttpStatus.SC_OK)
+                .body("bucket_id", is(bucket.getId().intValue()))
+                .body("id", is(workflow.id.intValue()))
+                .body("name", is("Valid Workflow"))
+                .body("project_name", is("Project Name"))
+                .body("revision", is(1))
+                .body("generic_information", hasSize(2))
+                .body("generic_information[0].key", is("genericInfo1"))
+                .body("generic_information[0].value", is("genericInfo1Value"))
+                .body("generic_information[1].key", is("genericInfo2"))
+                .body("generic_information[1].value", is("genericInfo2Value"))
+                .body("variables", hasSize(2))
+                .body("variables[0].key", is("var1"))
+                .body("variables[0].value", is("var1Value"))
+                .body("variables[1].key", is("var2"))
+                .body("variables[1].value", is("var2Value"));
+    }
+
+    @Test
+    public void testGetWorkflowPayloadShouldReturnSavedXmlPayload() throws IOException {
+        Response response =
+                given().pathParam("bucketId", 1).pathParam("workflowId", 1)
+                        .when().get(WORKFLOW_RESOURCE + "?alt=payload");
+
+        Arrays.equals(
+                ByteStreams.toByteArray(response.asInputStream()),
+                workflowRepository.getMostRecentWorkflowRevision(1L, 1L).getXmlPayload());
+
+        response.then()
+                .assertThat().statusCode(HttpStatus.SC_OK)
+                .contentType("application/xml");
+    }
+
+    @Test
+    public void testGetWorkflowMetadataShouldReturnReturnNotFoundIfNonExistingBucketId() {
+        given().pathParam("bucketId", 42).pathParam("workflowId", 1)
+                .when().get(WORKFLOW_RESOURCE).then()
+                .assertThat().statusCode(HttpStatus.SC_NOT_FOUND);
+    }
+
+    @Test
+    public void testGetWorkflowPayloadShouldReturnReturnNotFoundIfNonExistingBucketId() {
+        given().pathParam("bucketId", 42).pathParam("workflowId", 1)
+                .when().get(WORKFLOW_RESOURCE + "?alt=payload").then()
+                .assertThat().statusCode(HttpStatus.SC_NOT_FOUND);
+    }
+
+    @Test
+    public void testGetWorkflowMetadataShouldReturnReturnNotFoundIfNonExistingWorkflowId() {
+        given().pathParam("bucketId", 1).pathParam("workflowId", 42)
+                .when().get(WORKFLOW_RESOURCE).then()
+                .assertThat().statusCode(HttpStatus.SC_NOT_FOUND);
+    }
+
+    @Test
+    public void testGetWorkflowPayloadShouldReturnReturnNotFoundIfNonExistingWorkflowId() {
+        given().pathParam("bucketId", 1).pathParam("workflowId", 42)
+                .when().get(WORKFLOW_RESOURCE + "?alt=payload").then()
                 .assertThat().statusCode(HttpStatus.SC_NOT_FOUND);
     }
 
