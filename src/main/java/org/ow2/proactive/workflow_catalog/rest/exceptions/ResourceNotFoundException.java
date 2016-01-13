283f3a656c08ec13c96c84fbfcc0c280bcc6ad1f
Laurent Pellegrino <laurent.pellegrino@activeeon.com>
2016-01-13 14:39:15 +0100

    Remove unused constructor from exception classes

--- src/main/java/org/ow2/proactive/workflow_catalog/rest/exceptions/ResourceNotFoundException.java
+++ src/main/java/org/ow2/proactive/workflow_catalog/rest/exceptions/ResourceNotFoundException.java
@@ -39,23 +39,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 
-    public ResourceNotFoundException() {
-    }
-
     public ResourceNotFoundException(String message) {
         super(message);
-    }
-
-    public ResourceNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public ResourceNotFoundException(Throwable cause) {
-        super(cause);
-    }
-
-    public ResourceNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 
 }
--- src/main/java/org/ow2/proactive/workflow_catalog/rest/exceptions/UnprocessableEntityException.java
+++ src/main/java/org/ow2/proactive/workflow_catalog/rest/exceptions/UnprocessableEntityException.java
@@ -39,23 +39,12 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY)
 public class UnprocessableEntityException extends RuntimeException {
 
-    public UnprocessableEntityException() {
-    }
-
     public UnprocessableEntityException(String message) {
         super(message);
     }
 
-    public UnprocessableEntityException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
     public UnprocessableEntityException(Throwable cause) {
         super(cause);
-    }
-
-    public UnprocessableEntityException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 
 }
