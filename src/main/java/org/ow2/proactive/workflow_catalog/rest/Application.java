826d0a4df802e2ba266f9a701b6759c6544b3d8c
Laurent Pellegrino <laurent.pellegrino@gmail.com>
2016-01-14 20:38:09 +0100

    Remove unused method definition

--- src/main/java/org/ow2/proactive/workflow_catalog/rest/Application.java
+++ src/main/java/org/ow2/proactive/workflow_catalog/rest/Application.java
@@ -35,7 +35,6 @@ import com.google.common.base.Predicate;
 import org.ow2.proactive.workflow_catalog.rest.entity.Bucket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Bean;
@@ -63,11 +62,6 @@ public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
     }
 
     @Bean
