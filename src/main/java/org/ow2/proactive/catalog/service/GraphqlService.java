3d0201c28ee43c83deef2ecc51b9453503328568
Shatalov <yaroslav.shatalov@activeeon.com>
2017-07-07 16:22:30 +0200

    try to add schema in idl format

--- src/main/java/org/ow2/proactive/catalog/service/GraphqlService.java
+++ src/main/java/org/ow2/proactive/catalog/service/GraphqlService.java
@@ -25,12 +25,15 @@
  */
 package org.ow2.proactive.catalog.service;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 import javax.annotation.PostConstruct;
 
-import org.ow2.proactive.catalog.graphql.schema.type.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -40,6 +43,10 @@ import graphql.ExecutionResult;
 import graphql.GraphQL;
 import graphql.schema.DataFetcher;
 import graphql.schema.GraphQLSchema;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
 import lombok.extern.log4j.Log4j2;
 
 
@@ -57,14 +64,35 @@ public class GraphqlService {
     private DataFetcher catalogObjectFetcher;
 
     @PostConstruct
-    public void init() {
-        graphql = GraphQL.newGraphQL(GraphQLSchema.newSchema()
-                                                  .query(Query.builder()
-                                                              .catalogObjectFetcher(catalogObjectFetcher)
-                                                              .build()
-                                                              .getType())
-                                                  .build())
-                         .build();
+    public void init() throws IOException {
+        SchemaParser schemaParser = new SchemaParser();
+        SchemaGenerator schemaGenerator = new SchemaGenerator();
+
+        String pathToFile = "D:\\Workspace\\IDEA\\catalog-generic\\src\\main\\resources\\graphql-idl\\catalogObjectSchema.graphqls";
+        String content = new String(Files.readAllBytes(Paths.get(pathToFile)));
+        System.out.println("The content" + content);
+
+        File schemaFile = new File(pathToFile);
+
+        TypeDefinitionRegistry typeRegistry = schemaParser.parse(schemaFile);
+        RuntimeWiring wiring = buildRuntimeWiring();
+        GraphQLSchema graphQLSchema = schemaGenerator.makeExecutableSchema(typeRegistry, wiring);
+
+        graphql = GraphQL.newGraphQL(graphQLSchema).build();
+
+        //        graphql = GraphQL.newGraphQL(GraphQLSchema.newSchema()
+        //                                                  .query(Query.builder()
+        //                                                              .catalogObjectFetcher(catalogObjectFetcher)
+        //                                                              .build()
+        //                                                              .getType())
+        //                                                  .build())
+        //                         .build();
+    }
+
+    RuntimeWiring buildRuntimeWiring() {
+        return RuntimeWiring.newRuntimeWiring()
+                            .type("Query", typeWiring -> typeWiring.dataFetcher("CatalogObjects", catalogObjectFetcher))
+                            .build();
     }
 
     public Map<String, Object> executeQuery(String query, String operationName, Object graphqlContext,
