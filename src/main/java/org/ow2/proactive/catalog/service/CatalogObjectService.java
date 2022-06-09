08a61232fb9560a53bf33a838999031001e918b0
Alexandre Falessi <alexandre.falessi@activeeon.com>
2022-06-09 11:22:48 -0400

    Support oracleDB limitation of max 1000 elements for new IN query

--- src/main/java/org/ow2/proactive/catalog/service/CatalogObjectService.java
+++ src/main/java/org/ow2/proactive/catalog/service/CatalogObjectService.java
@@ -73,6 +73,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.Lists;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -84,6 +85,9 @@ import lombok.extern.log4j.Log4j2;
 @Service
 @Transactional
 public class CatalogObjectService {
+
+    public static int ORACLEDB_MAX_IN_PARAMS = 1000;
+
     @Autowired
     private CatalogObjectRepository catalogObjectRepository;
 
@@ -534,7 +538,12 @@ public class CatalogObjectService {
     private List<CatalogObjectRevisionEntity> getCatalogObjects(String bucketName, List<String> catalogObjectsNames) {
         findBucketByNameAndCheck(bucketName);
 
-        return catalogObjectRevisionRepository.findDefaultCatalogObjectsByNameInBucket(bucketName, catalogObjectsNames);
+        return Lists.partition(catalogObjectsNames, ORACLEDB_MAX_IN_PARAMS)
+                    .stream()
+                    .map(partitionedCatalogObjectsName -> catalogObjectRevisionRepository.findDefaultCatalogObjectsByNameInBucket(bucketName,
+                                                                                                                                  catalogObjectsNames))
+                    .flatMap(List::stream)
+                    .collect(Collectors.toList());
     }
 
     public CatalogObjectMetadata delete(String bucketName, String name) throws CatalogObjectNotFoundException {
